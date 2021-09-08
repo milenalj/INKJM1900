28513cdf33ae6581e9fbeb380b388b944a3bf60f
Milos Malic <milosmalic@gmail.com>
2021-09-08 18:52:06 +0200

    Completed sum_for task

---
+++ Assignments/sum_for.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 18:47:17 2021
+
+@author: milos
+"""
+
+#Problem 3.4 Errors in summation
+
+s = 0
+M = 3
+for k in range(1, M+1):
+    s += 1/(2*k)**2
+
+print(s)
