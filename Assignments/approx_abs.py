7b8545584bb76cf444170f5b3209e8da9c3495b0
Milena Ljubisic <milena.ljubisic@gmail.com>
2021-10-12 13:53:45 +0200

    fixed array length

--- Assignments/approx_abs.py
+++ Assignments/approx_abs.py
@@ -12,19 +12,20 @@ import matplotlib.pyplot as plt
 
 x_values = np.linspace(-np.pi, np.pi, 101)
 n_values = [1, 2, 3, 4]
-y_values = []
+#y_values = []
 
 def abs_approx(x, n):
     s = 0 
     for n in n_values:
         s = s + (np.cos((2*n-1)*x)/(2*n-1)**2)
         value = np.pi/2 - (4/np.pi)*s
-        y_values.append(value)
+        y_values = np.array(value)
+        #y_values.append(value)
         n = n + 1
     return y_values
 
-print(abs_approx(x_values, 4))
-#print(len(abs_approx(x_values, 4)))
+print(abs_approx(x_values, 2))
+print(len(abs_approx(x_values, 2)))
 
 """
 
@@ -41,7 +42,7 @@ def exact_value(x):
     return np.absolute(x)
 
 plt.plot(x_values, exact_value(x_values), color='red', label='Exact value')
-#plt.plot(x_values, abs_approx(x_values, 1), color='red', label='Exact value')
+plt.plot(x_values, abs_approx(x_values, 4), color='blue', label='Exact value')
 
 #for n in [1, 2, 3, 4]:
     #plt.plot(x, abs_approx(x, n), label='N = 'str(k))
