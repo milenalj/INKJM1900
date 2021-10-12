fb033bd2c7249c41d1b1cbe28c411fcf5680624a
Milena Ljubisic <milena.ljubisic@gmail.com>
2021-10-12 22:57:21 +0200

    Excercise finding_pi

--- Assignments/finding_pi.py
+++ Assignments/finding_pi.py
@@ -6,10 +6,14 @@ Created on Tue Oct 12 20:17:53 2021
 """
 import numpy as np
 
+x0 = 3,14
 
 
 
 numpy_pi = np.pi
 
-
+print("-------------------------------------------------")
 print("The value of pi from numpy is: %.13f" %numpy_pi)
+print("-------------------------------------------------")
+print("The value of x0 is: %.13f"%x0)
+print("-------------------------------------------------")
