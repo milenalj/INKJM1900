a876697f7a6ee805d21f2d40a9d0bbd383960efb
Milos Malic <milosmalic@gmail.com>
2021-10-12 23:04:08 +0200

    Implementation of newton_pi

--- Assignments/finding_pi.py
+++ Assignments/finding_pi.py
@@ -6,14 +6,23 @@ Created on Tue Oct 12 20:17:53 2021
 """
 import numpy as np
 
-x0 = 3,14
+x0 = 3.14
 
+def newton_pi(x):
+    next_x = x - (np.sin(x)/np.cos(x))
+    return next_x
 
+x1 = newton_pi(x0)
+x2 = newton_pi(x1)
 
-numpy_pi = np.pi
+print(x0, x1, x2)
 
+#numpy_pi = np.pi
+
+"""
 print("-------------------------------------------------")
 print("The value of pi from numpy is: %.13f" %numpy_pi)
 print("-------------------------------------------------")
-print("The value of x0 is: %.13f"%x0)
+#print("The value of x0 is: %.13f"%x0)
 print("-------------------------------------------------")
+"""
