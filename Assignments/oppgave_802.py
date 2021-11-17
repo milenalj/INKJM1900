9a33557b6dbb566e7c291a7e9642f7932bdfd451
Milena Ljubisic <milena.ljubisic@gmail.com>
2021-11-17 18:53:53 +0100

    excercise 802

---
+++ Assignments/oppgave_802.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 17:23:34 2021
+
+@author: milenaljubisic
+"""
+
+K_a =1.50e-04
+volume_acid = 0.0100
+volume_base = 0.03352
+concentration_base = 0.500
+K_w = 1.00e-14
+V_total = volume_acid + volume_base
+
