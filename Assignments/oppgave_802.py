ba7d383f81fafb349077ed01f10b3a706790c789
Milos Malic <milosmalic@gmail.com>
2021-11-17 20:29:41 +0100

    Half way implemented formula

--- Assignments/oppgave_802.py
+++ Assignments/oppgave_802.py
@@ -1,3 +1,4 @@
+import numpy as np
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 17 17:23:34 2021
@@ -6,9 +7,20 @@ Created on Wed Nov 17 17:23:34 2021
 """
 
 K_a =1.50e-04
-volume_acid = 0.0100
-volume_base = 0.03352
-concentration_base = 0.500
+V_b = 0.03352 # volume base
+c_b = 0.500 # concentration base
+V_a = 0.0100 # volume acid
+c_a = 0.00309 # concentratoin acid
 K_w = 1.00e-14
-V_total = volume_acid + volume_base
+V_t = V_a + V_b
 
+
+def hidrogen_concentration_func():
+    numerator = -c_b*V_b+np.sqrt(c_b**2*V_b**2 + 4*V_t**2*K_w + 4*V_t*c_a*V_a*K_a - 4*V_t*c_a*V_a*K_a**2)
+    denominator = 4*V_t
+    return numerator/denominator
+
+
+test = hidrogen_concentration_func()
+
+print(test)
