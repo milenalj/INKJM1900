f6304abcf649114450c29d1029368655b61f0457
Milos Malic <milosmalic@gmail.com>
2021-12-12 19:33:21 +0100

    Recoded to fix input checks

--- Assignments/concentration.py
+++ Assignments/concentration.py
@@ -1,28 +1,43 @@
 import sys
+print(sys.version_info)
 
 M = 158  # molar mass KMnO4 in g/mol
 V = 1    # volume of solution in liters
 
 
-m = eval(input("Input mass of KMnO4 in grams or Enter to stop:"))
+user_input = input("Input mass of KMnO4 in grams or Enter to stop:")
 
 
-def concentration(m):    
-    if type(m) == str:
+def concentration(user_input):
+    konsentrasjon = 0
+    m = 0
+    has_exception = False;
+    
+    #Try to cast to integer
+    try:
+        m = int(user_input)
+    except ValueError:
+        has_exception = True
+    
+    #Try to cast to float if not integer
+    if (has_exception):
+        try:
+            m = float(user_input)
+            has_exception = False
+        except ValueError:
+            has_exception = True
+        
+    #Wasn't integer nor float - raise error and exit
+    if (has_exception):
         raise TypeError("The input must be a number")
-        print(quit)
-        quit()
-    if type(m) == float:
-        pass
-    if type(m) == int:
-        pass
-    elif m > 76:
+        sys.exit()
+        
+    if m > 76:
         raise ValueError("The solubilty of KMnO4 is 76 g/L, the solution is saturated, input a lower number!")
-        print(quit)
-        quit()
+        sys.exit()
     else:
         konsentrasjon = (m/M)/V
     return konsentrasjon
 
 
-print("The concetration (molarity) of the KMnO4 solution is ", concentration(m), "M.")
+print("The concetration (molarity) of the KMnO4 solution is ", concentration(user_input), "M.")
